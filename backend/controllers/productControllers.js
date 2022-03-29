bea8ba7cdb866a576b21093953dc665931424e8b
MohammadOwais10 <owais0945@gmail.com>
2022-03-29 21:59:50 +0530

    Get Product Details

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -18,6 +18,21 @@ exports.getAllProducts = async (req, res) => {
   });
 };
 
+//Get Product Details
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
 //Update Product
 exports.updateProduct = async (req, res, next) => {
   let product = Product.findById(req.params.id);
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,6 +4,7 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  getProductDetails,
 } = require("../controllers/productControllers");
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 router.route("/product/:id").put(updateProduct);
 router.route("/product/:id").delete(deleteProduct);
+router.route("/product/:id").get(getProductDetails);
 
 module.exports = router;
