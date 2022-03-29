e8c022658f0126d74adcf720fb817922931d2222
MohammadOwais10 <owais0945@gmail.com>
2022-03-29 21:51:05 +0530

    Delete Product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -37,3 +37,19 @@ exports.updateProduct = async (req, res, next) => {
     product,
   });
 };
+
+//Delete Product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  await product.remove();
+  res.status(200).json({
+    success: true,
+    message: "Product Delete Successfully",
+  });
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,11 +3,13 @@ const {
   getAllProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productControllers");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").delete(deleteProduct);
 
 module.exports = router;
