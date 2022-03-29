0892177759e0769085f07946d15ff510b3bf4fe8
MohammadOwais10 <owais0945@gmail.com>
2022-03-29 21:42:06 +0530

    Update Product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -17,3 +17,23 @@ exports.getAllProducts = async (req, res) => {
     products,
   });
 };
+
+//Update Product
+exports.updateProduct = async (req, res, next) => {
+  let product = Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   getAllProducts,
   createProduct,
+  updateProduct,
 } = require("../controllers/productControllers");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
