808bdd7a54d77b0b418fd713efedcb8ce8778da0
MohammadOwais10 <owais0945@gmail.com>
2022-03-30 17:35:33 +0530

    Cast Error in MongoDB

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,13 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
 
+  //Cast Error in MongoDB id error
+  if (err.name === `CastError`) {
+    const message = `Resource not found. Invalid:${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  //end CastError code
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
