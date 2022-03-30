21524710a8ef9f8be21f08aaf25ef0a2ab254b56
MohammadOwais10 <owais0945@gmail.com>
2022-03-30 17:29:13 +0530

    Uncaught Exception Error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+//Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception `);
+  process.exit(1);
+});
+
 //CONFIG
 dotenv.config({ path: "backend/config/config.env" });
 
