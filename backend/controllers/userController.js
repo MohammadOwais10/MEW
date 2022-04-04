3304104575388f234394b1d86ccc99cb3b8d5334
MohammadOwais10 <owais0945@gmail.com>
2022-04-04 16:02:24 +0530

    Get User Details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -129,3 +129,12 @@ exports.resetPassword = catchAsyncError(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+//Get User Details
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,7 +5,11 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
 } = require("../controllers/userController");
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -17,5 +21,7 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
 
 module.exports = router;
