fd3c4b800e860f4a390174b33e4cb501f334196a
MohammadOwais10 <owais0945@gmail.com>
2022-04-04 12:28:54 +0530

    Mongoose Duplicate Key Error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,12 @@ module.exports = (err, req, res, next) => {
   }
   //end CastError code
 
+  // Mongoose Duplicate Key Error-->example it work when we entered same email for register which is already exited
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
