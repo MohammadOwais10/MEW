5d985b0224cc0c2a65f065bc31fbefe8ca00f23a
MohammadOwais10 <owais0945@gmail.com>
2022-04-08 16:00:31 +0530

    Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
+  return (
+    <Router>
+      <Header />;
+    </Router>
+  );
 }
 
 export default App;
