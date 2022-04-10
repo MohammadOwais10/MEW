cfc2996df8e909221c96c800b4afb2f0590cc808
MohammadOwais10 <owais0945@gmail.com>
2022-04-10 11:10:41 +0530

    Loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import WebFont from "webfontloader";
 import React from "react";
 import Footer from "./component/layout/Footer/Footer.js";
 import Home from "./component/Home/Home.js";
+import Loader from "./component/layout/Loader/Loader";
 
 function App() {
   React.useEffect(() => {
@@ -20,6 +21,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/loader" element={<Loader />} />
       </Routes>
       <Footer />
     </Router>
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -4,10 +4,11 @@ import "./Home.css";
 import Product from "./Product.js";
 import { getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.products);
+  const { products, loading } = useSelector((state) => state.products);
 
   useEffect(() => {
     dispatch(getProduct());
@@ -15,20 +16,27 @@ const Home = () => {
 
   return (
     <Fragment>
-      <div className="banner">
-        <p>Welcome to Ecommerce Master</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <div className="banner">
+            <p>Welcome to Ecommerce Master</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-        <a href="#container">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
-      </div>
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-        {products && products.map((product) => <Product product={product} />)}
-      </div>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <Product product={product} />)}
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
