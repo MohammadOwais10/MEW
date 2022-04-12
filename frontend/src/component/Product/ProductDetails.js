80994b0db34059d5721c8c7f4e247b40094c1729
MohammadOwais10 <owais0945@gmail.com>
2022-04-12 12:50:29 +0530

    Product Details UI

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -4,6 +4,7 @@ import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getProductDetails } from "../../actions/productAction";
+import ReactStars from "react-rating-stars-component";
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -14,6 +15,14 @@ const ProductDetails = () => {
   useEffect(() => {
     dispatch(getProductDetails(id));
   }, [dispatch, id]);
+
+  const options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
+
   return (
     <Fragment>
       <div className="ProductDetails">
@@ -23,12 +32,50 @@ const ProductDetails = () => {
               product.images.map((item, i) => (
                 <img
                   className="CarouselImage"
-                  key={item.url}
+                  key={i}
                   src={item.url}
                   alt={`${i} Slide`}
                 />
               ))}
           </Carousel>
+        </div>
+
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+          <div className="detailsBlock-2">
+            <ReactStars {...options} />
+            <span className="detailsBlock-2-span">
+              {" "}
+              ({product.numOfReviews} Reviews)
+            </span>
+          </div>
+          <div className="detailsBlock-3">
+            <h1>{`₹${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input readOnly type="number" />
+                <button>+</button>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+
+            <p>
+              Status:
+              <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                {product.Stock < 1 ? "OutOfStock" : "InStock"}
+              </b>
+            </p>
+          </div>
+
+          <div className="detailsBlock-4">
+            Description : <p>{product.description}</p>
+          </div>
+
+          <button className="submitReview">Submit Review</button>
         </div>
       </div>
     </Fragment>
