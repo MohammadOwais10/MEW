062e2a4ff8d36cc6e05dac75b0d991b3d7a80e70
MohammadOwais10 <owais0945@gmail.com>
2022-04-13 11:38:54 +0530

    Header Update

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,42 +1,49 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
 import logo from "../../../images/logo.PNG";
-
-const options = {
-  burgerColorHover: "#eb4034",
-  logo,
-  logoWidth: "20vmax",
-  navColor1: "white",
-  logoHoverSize: "10px",
-  logoHoverColor: "#eb4034",
-  link1Text: "Home",
-  link2Text: "Products",
-  link3Text: "Contact",
-  link4Text: "About",
-  link1Url: "/",
-  link2Url: "/products",
-  link3Url: "/contact",
-  link4Url: "/about",
-  link1Size: "1.3vmax",
-  link1Color: "rgba(35, 35, 35,0.8)",
-  nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-end",
-  nav3justifyContent: "flex-start",
-  nav4justifyContent: "flex-start",
-  link1ColorHover: "#eb4034",
-  link1Margin: "1vmax",
-  profileIconUrl: "/login",
-  profileIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColor: "rgba(35, 35, 35,0.8)",
-  cartIconColor: "rgba(35, 35, 35,0.8)",
-  profileIconColorHover: "#eb4034",
-  searchIconColorHover: "#eb4034",
-  cartIconColorHover: "#eb4034",
-  cartIconMargin: "1vmax",
-};
+import { FaSearch, FaShoppingBag, FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
-  return <ReactNavbar {...options} />;
+  return (
+    <ReactNavbar
+      burgerColorHover="grey"
+      logo={logo}
+      logoWidth="10vmax"
+      navColor1="#ff6a00"
+      logoHoverSize="10px"
+      logoHoverColor="white"
+      link1Text="Home"
+      link2Text="Products"
+      link3Text="Contact"
+      link4Text="About"
+      link1Url="/"
+      link2Url="/products"
+      link3Url="/contact"
+      link4Url="/about"
+      link1Size="2vmax"
+      link1Color="white"
+      nav1justifyContent="flex-end"
+      nav2justifyContent="flex-end"
+      nav3justifyContent="flex-start"
+      nav4justifyContent="flex-start"
+      link1ColorHover="black"
+      link1Margin="1.3vmax"
+      profileIconUrl="/login"
+      profileIconColor="white"
+      searchIconColor="white"
+      cartIconColor="white"
+      profileIconColorHover="black"
+      searchIconColorHover="black"
+      cartIconColorHover="black"
+      cartIconMargin="1vmax"
+      searchIcon={true}
+      SearchIconElement={FaSearch}
+      cartIcon={true}
+      CartIconElement={FaShoppingBag}
+      profileIcon={true}
+      ProfileIconElement={FaUserAlt}
+    />
+  );
 };
 
 export default Header;
