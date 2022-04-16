6a5c6d1644d1ccbf375738a44cafb9f19e87967e
MohammadOwais10 <owais0945@gmail.com>
2022-04-16 18:26:38 +0530

    Fix Error (CreatedAt Date)

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,6 +36,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "user",
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
