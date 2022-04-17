71c519db0a543bf4297e3b90747b600b42dc83a8
MohammadOwais10 <owais0945@gmail.com>
2022-04-17 11:42:57 +0530

    Update Profile Image (Backend)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -171,6 +171,22 @@ exports.updateProfile = catchAsyncError(async (req, res, next) => {
     email: req.body.email,
   };
 
+  /**------->Update Profile * */
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+    const imageId = user.avatar.public_id;
+    await cloudinary.v2.uploader.destroy(imageId);
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
+
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
     runValidators: true,
