ab95087ad8743fbca439eaf21142dd00c0057d8f
MohammadOwais10 <owais0945@gmail.com>
2022-04-18 17:06:21 +0530

    Cart Page (UI & Functionality)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import UpdateProfile from "./component/User/UpdateProfile.js";
 import UpdatePassword from "./component/User/UpdatePassword.js";
 import ForgotPassword from "./component/User/ForgotPassword.js";
 import ResetPassword from "./component/User/ResetPassword.js";
+import Cart from "./component/Cart/Cart.js";
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -56,6 +57,7 @@ function App() {
         />
         <Route path="/password/forgot" element={<ForgotPassword />} />
         <Route path="/password/reset/:token" element={<ResetPassword />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </Router>
