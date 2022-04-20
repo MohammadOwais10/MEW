e5e7a7e9addf9389c96ff40b59bab6c4e0b36b52
MohammadOwais10 <owais0945@gmail.com>
2022-04-20 10:28:38 +0530

    Payment Functionality in Backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+//CONFIG
+dotenv.config({ path: "backend/config/config.env" });
 
 app.use(express.json());
 app.use(cookieParser());
@@ -14,11 +18,13 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 const { urlencoded } = require("body-parser");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1/", order);
+app.use("/api/v1/", payment);
 
 // Middleware For Errors
 app.use(errorMiddleware);
---
+++ backend/controllers/paymentController.js
@@ -0,0 +1,21 @@
+const catchAsyncError = require("../middleware/catchAsyncError");
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.processPayment = catchAsyncError(async (req, res, next) => {
+  const myPayment = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "inr",
+    metadata: {
+      company: "Ecommerce Master",
+    },
+  });
+
+  res
+    .status(200)
+    .json({ success: true, client_secret: myPayment.client_secret });
+});
+
+exports.sendStripeApiKey = catchAsyncError(async (req, res, next) => {
+  res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+});
