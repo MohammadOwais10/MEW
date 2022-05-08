77e6d961955b3d331e90ff697171ec8c4da9c92b
MohammadOwais10 <owais0945@gmail.com>
2022-05-08 11:43:10 +0530

    Fix Bug (Category Filter)

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -32,20 +32,13 @@ import {
 } from "../constants/productConstant";
 
 export const getProduct =
-  (
-    keyword = "",
-    currentPage = 1,
-    price = [0, 200000],
-    category,
-    ratings = 0,
-    size
-  ) =>
+  (keyword = "", currentPage = 1, price = [0, 99999], category, ratings = 0) =>
   async (dispatch) => {
     try {
       dispatch({ type: ALL_PRODUCT_REQUEST });
       let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
-      if ((category, size)) {
-        link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}&size=${size}`;
+      if (category) {
+        link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
       }
 
       const { data } = await axios.get(link);
