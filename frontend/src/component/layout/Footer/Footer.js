63f56a43e6d578b4a6a9eed11e30c7c562436c40
MohammadOwais10 <owais0945@gmail.com>
2022-05-13 19:23:10 +0530

    Update Footer

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -1,31 +1,69 @@
 import React from "react";
-import playStore from "../../../images/playstore.png";
-import appStore from "../../../images/Appstore.png";
 import "./Footer.css";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import { Link } from "react-router-dom";
+import AttractionsIcon from "@mui/icons-material/Attractions";
+import AddIcCallIcon from "@mui/icons-material/AddIcCall";
+import CategoryIcon from "@mui/icons-material/Category";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const Footer = () => {
   return (
     <footer id="footer">
-      <div className="leftFooter">
-        <h4>DOWNLOAD OUR APP</h4>
-        <p>Download App for Android and IOS mobile phone</p>
-        <img src={playStore} alt="playstore" />
-        <img src={appStore} alt="Appstore" />
-      </div>
+      <div className="footerContainer">
+        <div className="leftFooter">
+          <h4>Quick's Link</h4>
+          <Link className="quickLink" to="/about">
+            <AttractionsIcon />
+            About us
+          </Link>
+          <Link className="quickLink" to="/contact">
+            <AddIcCallIcon />
+            Contact us
+          </Link>
+          <Link className="quickLink" to="/products">
+            <CategoryIcon />
+            Products
+          </Link>
+          <Link className="quickLink" to="/login">
+            <ExitToAppIcon />
+            SignIn
+          </Link>
+        </div>
 
-      <div className="midFooter">
-        <h1>Ecommerce Master</h1>
-        <p>High Quality is our first priority</p>
+        <div className="midFooter">
+          <h1>MEW</h1>
+          <h3>Men's Ethnic Wear</h3>
+          <p>
+            MEW is a marketplace model to capture the regional and traditional
+            variations of India.
+          </p>
+          <p>"'Discover Ethnic Indian Products'"</p>
+          <p className="copy">Copyrights 2022 &copy;MEW </p>
+        </div>
 
-        <p>Copyrights 2022 &copy;Ecommerce Master </p>
-      </div>
-
-      <div className="rightFooter">
-        <h4>Follow Us</h4>
-        <a href="https://twitter.com/">Twitter</a>
-        <a href="http://youtube.com">Youtube</a>
-        <a href="http://instagram.com">Instagram</a>
-        <a href="http://facebook.com">Facebook</a>
+        <div className="rightFooter">
+          <h4>Follow Us</h4>
+          <a href="https://twitter.com/">
+            <TwitterIcon className="twitterSvgIcon" />
+            Twitter
+          </a>
+          <a href="http://youtube.com">
+            <YouTubeIcon className="youtubeSvgIcon" />
+            Youtube
+          </a>
+          <a href="http://instagram.com">
+            <InstagramIcon className="instagramSvgIcon" />
+            Instagram
+          </a>
+          <a href="http://facebook.com">
+            <FacebookIcon className="facebookSvgIcon" />
+            Facebook
+          </a>
+        </div>
       </div>
     </footer>
   );
