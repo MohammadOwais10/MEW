17101789d0879569b431b7bbe093d1bdd32972d0
MohammadOwais10 <owais0945@gmail.com>
2022-12-02 19:30:49 +0530

    Header Material UI

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -9,58 +9,31 @@ import {
   Link,
   MenuItem,
 } from "@material-ui/core";
+
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import Badge from "@mui/material/Badge";
 import MenuIcon from "@material-ui/icons/Menu";
 import React, { useState, useEffect } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import LoginIcon from "@mui/icons-material/Login";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import mewLogo2 from "../../../images/mew logo2.PNG";
-const headersData = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Products",
-    href: "/products",
-  },
-  {
-    label: "About",
-    href: "/about",
-  },
-  {
-    label: "Contact",
-    href: "/contact",
-  },
-  {
-    label: "Search",
-    href: "/search",
-  },
-  {
-    label: (
-      <Button
-        variant="contained"
-        style={{
-          backgroundColor: "white",
-          fontSize: "16px",
-          fontWeight: 600,
-        }}
-        endIcon={<LoginIcon />}
-      >
-        Account
-      </Button>
-    ),
-    href: "/login",
-  },
-];
+import { useSelector } from "react-redux";
+import HomeIcon from "@mui/icons-material/Home";
+import CategoryIcon from "@mui/icons-material/Category";
+import SearchIcon from "@mui/icons-material/Search";
+import AttractionsIcon from "@mui/icons-material/Attractions";
+import LocalPostOfficeIcon from "@mui/icons-material/LocalPostOffice";
+import Tooltip from "@mui/material/Tooltip";
+import Zoom from "@mui/material/Zoom";
 
 const useStyles = makeStyles(() => ({
   header: {
     background: "linear-gradient(to right, rgb(255, 210, 0) 50%, #fe8c00)",
-    paddingRight: "75px",
+    paddingRight: "0px",
     paddingLeft: "0px",
     zIndex: 8,
     "@media (max-width: 900px)": {
-      paddingLeft: 0,
+      // paddingLeft: 0,
     },
   },
   logo: {
@@ -84,14 +57,159 @@ const useStyles = makeStyles(() => ({
   toolbar: {
     display: "flex",
     justifyContent: "space-between",
+    background: "linear-gradient(to right, rgb(255, 210, 0) 50%, #fe8c00)",
   },
   drawerContainer: {
-    padding: "20px 30px",
+    padding: "10px",
   },
 }));
 
 export default function Header() {
   const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const headersData = [
+    {
+      label: (
+        <Tooltip title="Home" arrow TransitionComponent={Zoom}>
+          <HomeIcon style={{ fontSize: "35PX" }} />
+        </Tooltip>
+      ),
+      href: "/",
+    },
+    {
+      label: (
+        <Tooltip title="Products" arrow TransitionComponent={Zoom}>
+          <CategoryIcon style={{ fontSize: "35PX" }} />
+        </Tooltip>
+      ),
+      href: "/products",
+    },
+    {
+      label: (
+        <Tooltip title="About" arrow TransitionComponent={Zoom}>
+          <AttractionsIcon style={{ fontSize: "35PX" }} />
+        </Tooltip>
+      ),
+      href: "/about",
+    },
+    {
+      label: (
+        <Tooltip title="Contact" arrow TransitionComponent={Zoom}>
+          <LocalPostOfficeIcon style={{ fontSize: "35PX" }} />
+        </Tooltip>
+      ),
+      href: "/contact",
+    },
+    {
+      label: (
+        <Tooltip title="Search" arrow TransitionComponent={Zoom}>
+          <SearchIcon style={{ fontSize: "35PX" }} />
+        </Tooltip>
+      ),
+      href: "/search",
+    },
+
+    {
+      label: (
+        <Tooltip title="Cart" arrow TransitionComponent={Zoom}>
+          <Badge badgeContent={cartItems.length} color="error">
+            <LocalMallIcon
+              style={{
+                fontSize: "35PX",
+                color: cartItems.length > 0 ? "black" : "unset",
+              }}
+            />
+          </Badge>
+        </Tooltip>
+      ),
+      href: "/cart",
+    },
+    {
+      label: (
+        <Tooltip title="Account" arrow TransitionComponent={Zoom}>
+          <AccountCircleIcon style={{ fontSize: "35PX" }} />
+        </Tooltip>
+      ),
+      href: "/login",
+      name: "Dashboard",
+    },
+  ];
+
+  const headersDataMobile = [
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <HomeIcon style={{ fontSize: "35PX" }} />
+          Home
+        </IconButton>
+      ),
+      href: "/",
+    },
+
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <AttractionsIcon style={{ fontSize: "35PX" }} />
+          About
+        </IconButton>
+      ),
+      href: "/about",
+    },
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <LocalPostOfficeIcon style={{ fontSize: "35PX" }} />
+          Contact
+        </IconButton>
+      ),
+      href: "/contact",
+    },
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <CategoryIcon style={{ fontSize: "35PX" }} />
+          Product
+        </IconButton>
+      ),
+      href: "/products",
+    },
+
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <SearchIcon style={{ fontSize: "35PX" }} />
+          Search
+        </IconButton>
+      ),
+      href: "/search",
+    },
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <Badge badgeContent={cartItems.length} color="error">
+            <LocalMallIcon
+              style={{
+                fontSize: "35PX",
+                color: cartItems.length > 0 ? "black" : "unset",
+              }}
+            />
+          </Badge>
+          Cart
+        </IconButton>
+      ),
+      href: "/cart",
+    },
+    {
+      label: (
+        <IconButton style={{ color: "white", fontWeight: "bold" }}>
+          <AccountCircleIcon style={{ fontSize: "35PX" }} />
+          Account
+        </IconButton>
+      ),
+      href: "/login",
+    },
+  ];
 
   const [state, setState] = useState({
     mobileView: false,
@@ -142,7 +260,7 @@ export default function Header() {
             onClick: handleDrawerOpen,
           }}
         >
-          <MenuIcon />
+          <MenuIcon style={{ fontSize: "35PX" }} />
         </IconButton>
 
         <Drawer
@@ -152,16 +270,18 @@ export default function Header() {
             onClose: handleDrawerClose,
           }}
         >
-          <div className={drawerContainer}>{getDrawerChoices()}</div>
+          <div className={drawerContainer}>
+            {mobileSideBarLogo}
+            {getDrawerChoices()}
+          </div>
         </Drawer>
-
         <div>{femmecubatorLogo}</div>
       </Toolbar>
     );
   };
 
   const getDrawerChoices = () => {
-    return headersData.map(({ label, href }) => {
+    return headersDataMobile.map(({ label, href }) => {
       return (
         <Link
           {...{
@@ -185,6 +305,21 @@ export default function Header() {
           style={{
             height: 80,
             width: 150,
+          }}
+          src={mewLogo2}
+          alt="Logo"
+        />
+      </Typography>
+    </Link>
+  );
+
+  const mobileSideBarLogo = (
+    <Link href="/">
+      <Typography>
+        <img
+          style={{
+            height: 100,
+            width: 200,
           }}
           src={mewLogo2}
           alt="Logo"
